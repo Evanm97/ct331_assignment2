479056a0c228e23044a6ad775115aa5e932b2407
Evan <evanmurray2012@gmail.com>
2018-11-08 12:38:53 +0000

    Update assignment_q1.rkt

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+; --Question 1--
+
+(cons 1 2)
+
+(cons 1 (cons 2 (cons 3 '())))
+
+(cons "a string" (cons 1 (cons '(1 2 3)'())))
+
+(list '"a string" '1 '(1 2 3))
+
+(append '("a string") '(1) '(((1 2 3))) )
